549b96fe63fcbb9058a54016d27d6b9fcd390049
Patsagorn Yuenyong <works@ptsgrn.dev>
2026-05-06 20:45:25 +0700

    chore: update dependencies in package.json and pnpm-lock.yaml

--- packages/api/src/index copy.ts
+++ packages/api/src/index copy.ts
@@ -5,56 +5,19 @@ import { devRoutes } from "./routes/dev/route";
 import { electionRoutes } from "./routes/election/route";
 import cors from "@elysiajs/cors";
 
-const swaggerOptions = (): ElysiaSwaggerConfig<"/reference"> => {
-	return {
-		path: "/reference",
-		exclude: ["/dev"],
-		documentation: {
-			info: {
-				version: "2568",
-				title: "SMO Vidya Election API",
-				contact: {
-					name: "SMO Vidya Election Backend Team",
-					url: "https://github.com/smovidya/smovidya-election-backend",
-				},
-			},
-			security: [{ bearerAuth: [] }],
-			components: {
-				securitySchemes: {
-					bearer: {
-						type: "http",
-						scheme: "bearer",
-						bearerFormat: "JWT",
-						description: "Google ID Token from Firebase Authentication",
-					},
-					...(env.ENVIRONMENT === "dev" && {
-						basic: {
-							type: "http",
-							scheme: "basic",
-							description:
-								"[TEST MODE ONLY] Basic Auth where `username` is mock student ID and `password` (optional) is mock current date/time in that parsable by JS's Date()",
-						},
-					}),
-				},
-			},
-		},
-	};
-};
-
 const app = new Elysia({
-	aot: false,
+  aot: false,
 })
-	.use(
-		cors({
-			aot: false,
-			origin: env.SITE_URL,
-		}),
-	)
-	.use(swagger(swaggerOptions()))
-	.use(electionRoutes);
+  .use(
+    cors({
+      aot: false,
+      origin: env.SITE_URL,
+    }),
+  )
+  .use(electionRoutes);
 
 if (env.ENVIRONMENT === "dev") {
-	app.mount("/dev", devRoutes);
+  app.mount("/dev", devRoutes);
 }
 
 export default app;
