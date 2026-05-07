d1b0ca504940fb81cc2f8001cd5c673992a50201
Ongsalt <ongsa.srp.mkm@gmail.com>
2026-05-07 22:31:17 +0700

    fix: ensure window object is available before checking login status

--- apps/frontend/src/components/MainCard.tsx
+++ apps/frontend/src/components/MainCard.tsx
@@ -73,7 +73,7 @@ export default function MainCard({
 
   const t = i18n[lang];
 
-  const isLoggedIn = localStorage.getItem("session_token") != null;
+  const isLoggedIn = window && localStorage.getItem("session_token") != null;
 
   useEffect(() => {
     const start = new Date(votingStartString);
